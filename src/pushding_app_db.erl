0b604c4bc7919e304ba62de071a7d3ebec574583
Stefan Strigler <stefan.strigler@erlang-solutions.com>
2016-02-15 21:16:59 +0000

    add create_app/2

--- src/pushding_app_db.erl
+++ src/pushding_app_db.erl
@@ -1,7 +1,8 @@
 -module(pushding_app_db).
 
 -export([init/0,
-         check_token/2]).
+         check_token/2,
+         create_app/2]).
 
 -record(pushding_app, {app_id :: binary(),
                        app_token :: binary(),
@@ -17,3 +18,10 @@ check_token(AppId, Token) ->
         [#pushding_app{app_token=Token}] -> true;
         _ -> false
     end.
+
+create_app(AppId, Token) when is_binary(AppId) and is_binary(Token) ->
+    mnesia:dirty_write(#pushding_app{app_id=AppId, app_token=Token});
+create_app(AppId, Token) ->
+    create_app(list_to_binary(AppId), list_to_binary(Token)).
+
+    
