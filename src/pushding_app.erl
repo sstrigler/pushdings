b9bfd075e3a8d1b7f89fc1aa3a12c0c002d211ab
Stefan Strigler <stefan@strigler.de>
2016-02-15 18:06:47 +0000

    start cowboy dispatcher

--- src/pushding_app.erl
+++ src/pushding_app.erl
@@ -5,6 +5,12 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [{"/", pushding_rest_handler, []}]}
+    ]),
+    cowboy:start_http(pushding_http_listener, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
     pushding_sup:start_link().
 
 stop(_State) ->
