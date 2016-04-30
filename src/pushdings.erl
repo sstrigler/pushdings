d89ba780345f28d102bc4e73c74782785e904b0c
Stefan Strigler <stefan@strigler.de>
2016-04-30 10:27:30 +0200

    add restart
    also regroup

--- src/pushdings.erl
+++ src/pushdings.erl
@@ -7,7 +7,9 @@
          lift/1,
          num_subscriptions/1,
          publish/2,
+         restart/0,
          start/0,
+         stop/0,
          subscribe/1]).
 
 -export([debug/1,
@@ -20,6 +22,7 @@
          warning/2]).
 
 %% ----------------------------- < application > -------------------------------
+
 start(_Type, _Args) ->
     mnesia:wait_for_tables(
       pushdings_application:tables() ++
@@ -28,12 +31,12 @@ start(_Type, _Args) ->
     Dispatch = cowboy_router:compile([
         {'_', [
                {"/", cowboy_static, {priv_file, pushdings, "index.html"}},
-               {"/applications", pushdings_applications_handler, []},
-               {"/applications/:id", pushdings_application_handler, []},
-               {"/messages", pushdings_messages_handler, []},
-               {"/registrations/:email", pushdings_registration_handler, []},
-               {"/registrations", pushdings_registrations_handler, []},
-               {"/ws", pushdings_ws_handler, []}
+               {"/applications",      pushdings_applications_handler, []},
+               {"/applications/:id",  pushdings_application_handler, []},
+               {"/messages",          pushdings_messages_handler, []},
+               {"/registrations/:id", pushdings_registration_handler, []},
+               {"/registrations",     pushdings_registrations_handler, []},
+               {"/ws",                pushdings_ws_handler, []}
               ]}]),
 
     {ok, _Pid} = cowboy:start_http(pushdings_http_listener, 100,
@@ -46,14 +49,24 @@ start(_Type, _Args) ->
 stop(_State) -> cowboy:stop_listener(pushdings_http_listener).
 
 %% --------------------------------- < api > -----------------------------------
+
 install(Nodes) ->
     ok = mnesia:create_schema(Nodes),
     ok = application:start(mnesia),
     pushdings_application:install(Nodes),
     pushdings_registration:install(Nodes).
 
+%% ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 
-start() -> application:ensure_all_started(pushdings).
+restart() ->
+    application:stop(?MODULE),
+    application:start(?MODULE).
+
+start() -> application:ensure_all_started(?MODULE).
+
+stop() -> application:stop(?MODULE).
+
+%% ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 
 publish(Topic, Message) -> gproc_ps:publish(l, {?MODULE, Topic}, Message).
 
@@ -61,11 +74,14 @@ subscribe(Topic) -> gproc_ps:subscribe(l, {?MODULE, Topic}).
 
 num_subscriptions(Topic) -> length(gproc_ps:list_subs(l, {?MODULE, Topic})).
 
+%% ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
 lift(Error) when is_tuple(Error), element(1, Error) == error -> Error;
 lift(Ok)    when is_tuple(Ok), element(1, Ok) == ok          -> Ok;
 lift(Other)                                                     -> {ok, Other}.
 
-%% -------------------------------- < lager > ----------------------------------
+%% ----- ----- ----- ----- ----- -- < lager > ---- ----- ----- ----- ----- -----
+
 -define(LAGER1(F), F(X)    -> ok = lager:F(X)).
 -define(LAGER2(F), F(X, Y) -> ok = lager:F(X, Y)).
 
